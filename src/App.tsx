59ad4bc29a1164390e781837e405a3a7f2c7aae7
fredabel <tuazonpred@gmail.com>
2025-08-18 12:19:18 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import Profile from './pages/Profile';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+
+
+import { AuthProvider } from './context/AuthContext';
+
+function App() {
+
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          {/* <Navbar /> */}
+          <Routes>
+            {/* <Route path='/' element={ <Home/> } />
+            <Route path='/login' element={ <Login/> } />
+            <Route path='/register' element={ <Register/> } />
+            <Route path='/profile' element={ <Profile/> } /> */}
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  )
+}
+
+export default App
